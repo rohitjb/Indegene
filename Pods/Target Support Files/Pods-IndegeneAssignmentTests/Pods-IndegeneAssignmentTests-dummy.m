ab6520fa163abee4c11abeccc6b370bcfeed4f4f
Rohit Sharma <rohit0690@gmail.com>
2018-04-17 10:43:07 +0530

    Removing pods completley

--- Pods/Target Support Files/Pods-IndegeneAssignmentTests/Pods-IndegeneAssignmentTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IndegeneAssignmentTests : NSObject
-@end
-@implementation PodsDummy_Pods_IndegeneAssignmentTests
-@end
--- Pods/Target Support Files/Pods-IndegeneAssignmentUITests/Pods-IndegeneAssignmentUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IndegeneAssignmentUITests : NSObject
-@end
-@implementation PodsDummy_Pods_IndegeneAssignmentUITests
-@end
